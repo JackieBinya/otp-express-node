97a9aa9f06c272d3cfa4203aa062d4baf03d165c
JackieBinya <jacquelinebinya@gmail.coom>
2023-01-31 02:45:39 +0000

    Adds OTP Model

---
+++ api/src/models/Otp.js
@@ -0,0 +1,30 @@
+import { Model, DataTypes }  from 'sequelize';
+import db from '../db/index.js'
+
+class OPT extends Model {}
+
+OTP.init({
+  id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+   },
+  otp: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  ip: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+}, {
+  timestamps: true,
+  sequelize: db,
+  modelName: 'otp' 
+});
+
+export default OTP;
