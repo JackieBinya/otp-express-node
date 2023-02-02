05c45e1e877f78078ccd2b88541bbfbe79063834
JackieBinya <jacquelinebinya@gmail.coom>
2023-02-02 02:50:48 +0000

    Breakdown main scripts to individual scripts for each view

--- public/js/createOtp.js
+++ public/js/createOtp.js
@@ -1,11 +1,7 @@
 const createOtpForm = document.querySelector("#createOtpForm");
-const verifyOtpForm = document.querySelector("#verifyOtpForm");
 
 const createOtpFormSuccessMessage = createOtpForm.querySelectorAll("span.success-message")[0];
 const createOtpFormErrorMessage = createOtpForm.querySelectorAll("span.error-message")[0];
-
-const verifyOtpFormSuccessMessage = createOtpForm.querySelectorAll("span.success-message")[0];
-const verifyOtpFormErrorMessage = createOtpForm.querySelectorAll("span.error-message")[0];
 
 createOtpForm.addEventListener("submit", async(e) => {
     try {
@@ -37,38 +33,5 @@ createOtpForm.addEventListener("submit", async(e) => {
     } catch (error) {
       console.log({error});
       createOtpFormErrorMessage.textContent ='Something happened, please try again later!';
-    }
-  });
-
-  verifyOtpForm.addEventListener("submit", async(e) => {
-    try {
-      e.preventDefault();
-
-    const formData = new FormData(createOtpForm);
-
-    const data = Object.fromEntries(formData);
-
-    const response = await fetch('/otp/verify',{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-
-    const res = await response.json();
-
-    if(!res.success){
-      verifyOtpFormErrorMessage.textContent = res.error;
-    }
-
-    if(res.success){
-      verifyOtpForm.reset();
-      verifyOtpFormSuccessMessage.textContent ='Your OTP has been successfully verified🎊.';
-    }
-   
-    } catch (error) {
-      console.log({error});
-      verifyOtpFormErrorMessage.textContent ='Something happened, please try again later!';
     }
   });
--- routes/otp.views.routes.js
+++ routes/otp.views.routes.js
@@ -3,10 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {title: 'Entrostat - Generate OTP', script: '/js/createOtp.js'});
 });
 router.get('/verify-otp', (req, res) => {
-    res.render('verifyOtp');
+    res.render('verifyOtp', {title: 'Entrostat - Verify Otp', script: '/js/verifyOtp.js'});
 });
 
 export default router;
